7ac68de409ffc53aab5368ee17a9ced71e0ff9bf
Julien Raynal <julien.raynal@epsi.fr>
2020-03-10 16:09:37 +0100

    changement mineur de C.txt

---
+++ cmake-build-debug/CMakeFiles/C-hat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "C-hat"
+  "C-hat.pdb"
+  "CMakeFiles/C-hat.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C-hat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
