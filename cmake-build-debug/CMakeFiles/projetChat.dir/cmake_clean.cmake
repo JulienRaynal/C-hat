f0f9630f957960f2218fd0793191972007629a17
Guigui <guillaume.ros@epsi.fr>
2020-03-10 15:29:49 +0100

    Setup

---
+++ cmake-build-debug/CMakeFiles/projetChat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projetChat.dir/main.c.o"
+  "projetChat"
+  "projetChat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projetChat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
